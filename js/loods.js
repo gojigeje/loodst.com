8e4f9546f9a53d7ff2fbf82397f0ac63f4142944
Ghozy Arif Fajri <gojigeje@gmail.com>
2013-12-31 02:02:41 +0700

    First commit

---
+++ js/loods.js
@@ -0,0 +1,9 @@
+$(function () {
+  $('.twitter').click(function(){
+    window.open('//twitter.com/loodst','_blank');
+  });
+
+  $('.facebook').click(function(){
+    window.open('//facebook.com/loadingsystemplant','_blank');
+  });
+});
